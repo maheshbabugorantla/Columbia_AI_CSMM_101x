27fbddc95d76ce2e652ebcf7a4444ac0896031a9
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2020-03-02 07:16:11 -0600

    Added logic to compute max_ram_usage for a BFS instance and refactored the search method

--- Week2_Search_Algorithms/utility/bfs.py
+++ Week2_Search_Algorithms/utility/bfs.py
@@ -2,20 +2,28 @@
     Breadth First Search
 """
 from collections import deque
+from resource import getrusage, RUSAGE_SELF
 
 class BFS:
-    def __init__(self, start_state, goal_state):
+
+    def __init__(self, start_state, goal_state, start_ram_usage=0):
         self.start_state = start_state
         self.current_state = start_state
         self.goal_state = goal_state
         self.max_search_depth = 0
+        self.start_ram = start_ram_usage
+        self.max_ram_usage = 0
         self.queue = deque([(start_state, 0)])
 
     def _expand_current_node(self, current_depth=0):
         children = self.current_state.expand()
+
+        # Iterating the children keeps expanding the graph size in RAM
         for child in children:
             self.max_search_depth = max(self.max_search_depth, current_depth+1)
-            self.queue.append((child, current_depth+1))
+            self.queue.append((child, current_depth + 1))
+            current_ram_usage = getrusage(RUSAGE_SELF).ru_maxrss
+            self.max_ram_usage = max(self.max_ram_usage, current_ram_usage - self.start_ram)
 
     def is_goal(self):
         return self.current_state.config == self.goal_state
@@ -39,6 +47,12 @@ class BFS:
         actions_to_goal.reverse()
         return actions_to_goal
 
+    def get_max_ram_usage(self):
+        return self.max_ram_usage
+
+    def get_max_search_depth(self):
+        return self.max_search_depth
+
     def search(self, display_path=False):
 
         visited_nodes = set()
@@ -56,11 +70,9 @@ class BFS:
                     break
                 visited_nodes.add(current_state.config)
                 self._expand_current_node(current_depth=current_depth)
-
         self.queue.clear() # Empty the Queue
         path_to_goal = self._path_to_goal(display=display_path)
 
-
         if not goal_found:
-            return False, [], -1, nodes_expanded
-        return True, path_to_goal, self.current_state.cost, nodes_expanded-1, search_depth, self.max_search_depth
+            return False, [], self.current_state.cost, nodes_expanded, search_depth
+        return True, path_to_goal, self.current_state.cost, nodes_expanded-1, search_depth
