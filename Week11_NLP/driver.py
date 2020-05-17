bfeacf74f9dc65bdc1ff6eb6f2fd24041fa45f48
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2020-05-17 16:50:45 -0500

    Vocareum Submission First Try

--- Week11_NLP/driver.py
+++ Week11_NLP/driver.py
@@ -12,12 +12,12 @@ def write_results_to_file(filename, test_preds):
 def main():
 
     # Vocareum Paths
-    # train_path = "../resource/lib/publicdata/aclImdb/train"
-    # test_path = "../resource/lib/publicdata/imdb_te.csv"
+    train_path = "../resource/lib/publicdata/aclImdb/train"
+    test_path = "../resource/lib/publicdata/imdb_te.csv"
 
     # Local Machine Paths
-    train_path = "aclImdb/train"
-    test_path = "imdb_te.csv"
+    # train_path = "aclImdb/train"
+    # test_path = "imdb_te.csv"
 
     sentiment_classifier = TextClassifier(
         stop_words_file='stopwords.en.txt',
@@ -31,7 +31,7 @@ def main():
         unigram.output.txt
     '''
     sentiment_classifier.train_using_tfidf(n_gram=1)
-    predictions = sentiment_classifier.test_using_tfidf(n_gram=1)
+    predictions = sentiment_classifier.test_using_count(n_gram=1)
     write_results_to_file('unigram.output.txt', predictions)
 
     '''
@@ -39,18 +39,27 @@ def main():
         predict sentiments on imdb_te.csv, and write output to
         bigram.output.txt
     '''
+    sentiment_classifier.train_using_tfidf(n_gram=2)
+    predictions = sentiment_classifier.test_using_count(n_gram=2)
+    write_results_to_file('bigram.output.txt', predictions)
 
     '''
         train a SGD classifier using unigram representation
         with tf-idf, predict sentiments on imdb_te.csv, and write
         output to unigramtfidf.output.txt
     '''
+    sentiment_classifier.train_using_tfidf(n_gram=1)
+    predictions = sentiment_classifier.test_using_tfidf(n_gram=1)
+    write_results_to_file('unigramtfidf.output.txt', predictions)
 
     '''
         train a SGD classifier using bigram representation
         with tf-idf, predict sentiments on imdb_te.csv, and write
         output to bigramtfidf.output.txt
     '''
+    sentiment_classifier.train_using_tfidf(n_gram=2)
+    predictions = sentiment_classifier.test_using_tfidf(n_gram=2)
+    write_results_to_file('unigramtfidf.output.txt', predictions)
 
 
 if __name__ == "__main__":
